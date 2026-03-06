4303020509a1d5d21921cd69325faa496a4c2709
Shashank C <shashankshashankc39@gmail.com>
2026-03-06 15:57:37 +0530

    chore: cleanup project by removing testing logs and unrelated files

--- read_out.ts
+++
@@ -1,9 +0,0 @@
-import fs from 'fs';
-const text = fs.readFileSync('out_domains.txt', { encoding: 'utf16le' });
-const lines = text.split('\n');
-for (const line of lines) {
-    if (line.includes('[!] LISTED')) {
-        const parts = line.split(':');
-        console.log("LISTED ON:", parts[0].replace('[!] LISTED on ', ''));
-    }
-}
